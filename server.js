d9eaba756554c15e37cc7df834a024b9ffcdc1db
KGibbon <gibbon@furg.br>
2019-06-13 11:42:49 -0300

    (FIX) hello world para teste azure

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 var app = require('./config/custom-express')();
 
-app.listen(3000, function(){
-  console.log("Servidor rodando!");
+app.get('/', function(req, res) {
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<h1>Hello World</h1>');
+})
+
+app.listen(3000, function() {
+    console.log("Servidor rodando!");
 });
