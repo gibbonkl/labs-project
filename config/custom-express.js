3f759024ab4bea588a2e9fc04b0e08eee78bb060
KGibbon <gibbon@furg.br>
2019-06-13 12:35:24 -0300

    (FIX) route

--- config/custom-express.js
+++ config/custom-express.js
@@ -7,11 +7,11 @@ module.exports = function() {
 
     var app = express();
 
-    app.use(express.static('public/'));  
+    app.use(express.static('app/'));
 
-    consign({cwd: 'public'})
-    .include('controllers')
-    .into(app);
+    consign({ cwd: 'app' })
+        .include('routes')
+        .into(app);
 
     return app;
 }
