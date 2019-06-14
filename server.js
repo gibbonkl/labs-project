1b878a1eaca96d1304a6e59c2ce6388283b07ca8
KGibbon <gibbon@furg.br>
2019-06-14 15:39:13 -0300

    (FIX) porta variavel de ambiente

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var app = require('./config/custom-express')();
+const porta = process.env.PORT;
 
-app.listen(1337, function() {
-    console.log("Servidor rodando!");
+app.listen(porta || 1337, function() {
+  console.log("Servidor rodando na porta %d!", porta);
 });
