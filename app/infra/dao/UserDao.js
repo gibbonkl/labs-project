3fd1c1504f20ecd6127f1fe72a31fefc72d985e2
Diego Bastos da Costa <dbdcosta96@gmail.com>
2019-06-17 09:48:23 -0300

    Userdao: updatehash,updatepassword

--- app/infra/dao/UserDao.js
+++ app/infra/dao/UserDao.js
@@ -35,11 +35,18 @@ class UserDao extends TemplateDao{
             return this._findOne({$or: [{username:user.username},{email:user.email}]})
                 .then(res => res? res : null)
                 .catch(err=>{
-                    console.log(err);
+                    console.erro(err);
                     return({detail:"Impossível realizar operação.",error:err});
                 })
         }
         return({detail:"Impossível realizar operação",error:"Usuário null ou undefined"})
+    }
+    updateHash(username='',newHash='',hash=''){
+        if(username && newHash && hash){
+            return this._update({username:username,hash:hash},{$set:{hash: newHash}})
+                
+        }
+        return({detail:"Impossível realizar operação",error:"Campo inválido"})
     }
     /*
         *   Verifica se o username possui a hash determinada
@@ -47,19 +54,38 @@ class UserDao extends TemplateDao{
         *   @param {string} hash Hash do usuário
         *   @returns {bool}
     */
-    getHash(username,hash){
+    getHash(username='',hash=''){
         if(username && hash){
             return this._findOne({username: username,hash: hash})
                 .then(res=> res?true:false)
                 .catch(err=>{
-                    console.log(err);
+                    console.error(err);
                     return(false);
                 })
         }
         return(false);
     }
-    updatePassword(){
-
+    updatePassword(username="",newPassword=""){
+        if(username && newPassword){
+            return this._update({username:username},{$set:{senha: newPassword}})
+        }
+    }
+    /*
+        *   Busca por um usuário pelo username e senha
+        *   @param {string} username Nome de usuário
+        *   @param {string} password Senha de usuário
+        *   @returns {object}
+    */
+    login(username='',password=''){
+        if(username && password){
+            return this._findOne({username:username,senha:password})
+                .then(res=> res? res: null)
+                .catch(err=>{
+                    console.error(err);
+                    return({detail:"Impossível realizar autenticação.",error:err})
+                })
+        }
+        return({detail:"Impossível realizar autenticação.",error:"Username e senha null ou undefined"})
     }
 }
 module.exports = UserDao;
