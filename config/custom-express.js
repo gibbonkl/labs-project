d4067f6b4712245fbceb8a58b043553206e003e3
Gibbon <gibbon@furg.br>
2019-06-19 11:08:14 -0300

    (FIX) debug login

--- config/custom-express.js
+++ config/custom-express.js
@@ -12,9 +12,9 @@ module.exports = function() {
     app.use(express.static('app/'));
 
     app.set('view engine', 'ejs');
-    app.set('views', path.join(__dirname, '/../app/views/'));
+    app.set('views', path.join(__dirname, './../app/views/'));
     
-    console.log(path.join(__dirname + '/../app'));
+    console.log(path.join(__dirname + './../app'));
     
     app.use(express.json());
 
