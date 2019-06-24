a4c2bb353f04254eef839128a2000316d23e397b
Rafael-Romeu <rafaelromeufurg@gmail.com>
2019-06-24 19:58:01 -0300

    (FIX) separa rota cadastro do controller

--- app/controllers/controller_cadastra_usuario.js
+++ app/controllers/controller_cadastra_usuario.js
@@ -2,33 +2,44 @@ let Model = require("../models/schema_usuario");
 let UserDAO = require('../infra/dao/UserDao');
 let ModeloUsuarioCadastro = require('../models/modelo_usuario_cadastro');
 
-module.exports = new Promise()
-                .then((req) => {
-                    let user = new Model({
-                        nome: req.body.nome,
-                        sobrenome: req.body.sobrenome,
-                        username: req.body.username,
-                        email: req.body.email,
-                        senha: req.body.senha,
-                        imagem: req.body.imagem,
-                        data_nascimento: new Date(req.body.data_nasc)
-                    });
-                
-                    let userDAO = new UserDAO(Model);
-                    userDAO.insertUser(user)
-                        .then(user => {
-                            if(user && !user.error){
-                                return JSON.parse({"status" : "ok", "user" : user});
-                            }
-                            else 
-                            {
-                                let modeloUsuario = new ModeloUsuarioCadastro();
-                                modeloUsuario.preencheAutomatico(req.body);
-                                modeloUsuario.erroUsuarioExistente();
-                                return JSON.parse( {"status" : "error", "user" : modeloUsuario.getUser()});
-                            }
-                        })
-                        .catch(console.log);
+module.exports = function(req)
+{
+    console.log('controller cadastra usuario');
 
-                })
-                .catch();
+    let user = new Model({
+        nome: req.body.nome,
+        sobrenome: req.body.sobrenome,
+        username: req.body.username,
+        email: req.body.email,
+        senha: req.body.senha,
+        imagem: req.body.imagem,
+        data_nascimento: new Date(req.body.data_nasc)
+    });
+
+    let userDAO = new UserDAO(Model);
+    let promise = userDAO.insertUser(user)
+        .then(user => {
+            if(user && !user.error){
+                
+                return{"status" : "ok", "user" : user};
+            }
+            else 
+            {
+                let modeloUsuario = new ModeloUsuarioCadastro();
+                modeloUsuario.preencheAutomatico(req.body);
+                modeloUsuario.erroUsuarioExistente();
+
+                return {"status" : "error", "user" : modeloUsuario.getUser()};
+            }
+        })
+        .catch(() => {
+
+            let modeloUsuario = new ModeloUsuarioCadastro();
+            modeloUsuario.preencheAutomatico(req.body);
+            modeloUsuario.erroUsuarioExistente();
+
+            return {"status" : "error", "user" : modeloUsuario.getUser()};
+        });
+
+        return promise;
+}
--- app/routes/cadastro.js
+++ app/routes/cadastro.js
@@ -6,8 +6,7 @@ let controllerCadastraUsuario = require('../controllers/controller_cadastra_usua
 module.exports = function(app)
 {
     //middleware de validação
-    app.use('/cadastro', (req,res,next) => 
-    {
+    app.use('/cadastro', (req,res,next) => {
         if(req.method == 'POST')
         {
             console.log('Middleware Validação Cadastro');
@@ -33,43 +32,17 @@ module.exports = function(app)
             res.render('cadastro', { user : modeloUsuario.getUser()});
         })
         .post((req, res) => {
-            let retorno = controllerCadastraUsuario(req);
-            console.log(retorno);
-
-            // if( retorno.status == 'ok')
-            // {
-            //     req.session.user = retorno.user;
-            //     res.redirect('/dashboard');
-            // }else{
-            //     res.render('cadastro', { user : retorno.user});
-            // }
-
-            // let user = new Model({
-            //         nome: req.body.nome,
-            //         sobrenome: req.body.sobrenome,
-            //         username: req.body.username,
-            //         email: req.body.email,
-            //         senha: req.body.senha,
-            //         imagem: req.body.imagem,
-            //         data_nascimento: new Date(req.body.data_nasc)
-            // });
+            console.log('Rota Cadastro (metodo Post)');
             
-            // let userDAO = new UserDAO(Model);
-            // userDAO.insertUser(user)
-            //     .then(user => {
-            //         if(user && !user.error){
-            //             req.session.user = user;
-            //             res.redirect('/dashboard');
-            //         }
-            //         else 
-            //         {
-            //             let modeloUsuario = new ModeloUsuarioCadastro();
-            //             modeloUsuario.preencheAutomatico(req.body);
-            //             modeloUsuario.erroUsuarioExistente();
-            //             res.render('cadastro', { user : modeloUsuario.getUser()});
-            //         }
-            //     })
-            //     .catch(console.log);
+            controllerCadastraUsuario(req)
+                .then((retorno) => {
+                    if( retorno.status == 'ok')
+                    {
+                        req.session.user = retorno.user;
+                        res.redirect('/dashboard');
+                    }else{
+                        res.render('cadastro', { user : retorno.user});
+                    }
+                });
         });
 }
-
