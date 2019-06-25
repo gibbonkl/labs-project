c522a83a693fe852c03936eb462795fa8dadc858
Rafael Neves Romeu <rafael.romeu_BOLS@pampa.compasso>
2019-06-25 11:56:11 -0300

    (FEAT) recaptcha

---
+++ app/helper/recaptcha_verify.js
@@ -0,0 +1,4 @@
+var Recaptcha = require('express-recaptcha').RecaptchaV2;
+var recaptcha = new Recaptcha('6LcneqgUAAAAANgWtxI4eN1Gh6Jl2Jxw5dgK9tEB', '6LcneqgUAAAAAPxTTRQzAdMYBN1hf3ebdVjWAK6L');
+
+module.exports = recaptcha.middleware.verify;
