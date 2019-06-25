0f82b859691d599950066e542268eec9730365a9
Albano Borba <albano.b06@gmail.com>
2019-06-25 12:11:00 -0300

    (FIX) login notifcation

--- app/infra/dao/TemplateDao.js
+++ app/infra/dao/TemplateDao.js
@@ -57,9 +57,9 @@ class TemplateDao{
         *   @params {object} options Opções para a busca
         *   @returns {object}
     */
-    _find(filter={}, projection={},options={}){
+    _find(filter={}, projection={}, options={}){
         return this._model
-            .find(filter, projection,options)
+            .find(filter, projection, options)
             .exec();
     }
     /*
