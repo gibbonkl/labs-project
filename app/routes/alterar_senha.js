32c205b313a047f9356c75ac2c4ddb54666b07ec
Gibbon <gibbon@furg.br>
2019-06-25 10:52:35 -0300

    (FEAT) controller route alterar senha

--- app/routes/alterar_senha.js
+++ app/routes/alterar_senha.js
@@ -6,19 +6,49 @@ module.exports = function (app) {
     //middleware de validação
     app.use('/alterar_senha', (req, res, next) => {
         if (req.method == 'POST') {
-            //validar campos redirecionar pra alt senha
+
+            let user = {
+                senha: req.body.senha,
+                novasenha: req.body.novasenha,
+                confirmacaosenha: req.body.confirmacaosenha,
+                erros: [],
+                invalidClass: '',
+            } 
+
+            if (!user.senha || !user.novasenha || !user.confirmacaosenha || (user.novasenha != user.confirmacaosenha)) {
+
+                temErro = true;
+                user.invalidClass = 'invalid';
+
+                res.send("Impossível alteração de senha");
+            }
+            else {
+                next();
+            }
         }
         else {
             next();
         }
     });
-    // route for user signup
-    app.route('/alterar_senha')
-        .get(sessionChecker, (req, res) => {
-            res.render('alterar_senha');
-        })
-        .post((req, res) => {
-            //passar body da requisicao 
 
+    // rota para alterar senha
+    app.route('/alterar_senha')
+        .get( (req, res) => {
+            res.send(req.session.user.username);
+            //res.render('alterar_senha');
+        })
+        .post((req, res, next) => {
+            let user = {
+                senha: req.body.senha,
+                novasenha: req.body.novasenha,
+                confirmacaosenha: req.body.confirmacaosenha,
+            } 
+
+            //console.log(session.user);
+            
+            let userDAO = new UserDAO(Model);
+            return userDAO.updatePassword(username = req.session.user.username, newPassword = user.novasenha)
+                .then()
+                .catch((error) => res.send("Impossível modificação de senha", error))
         });
 }
