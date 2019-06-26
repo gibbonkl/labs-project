dbfda861420eb1c4eaa10382e9019561ed9918ec
Albano Borba <albano.b06@gmail.com>
2019-06-26 10:02:29 -0300

    (FEAT) controller

--- app/controllers/DashboardController.js
+++ app/controllers/DashboardController.js
@@ -1,5 +1,9 @@
 class DashboardController {
     constructor(){
-        
+        throw new Error("Classe estática. Impossível instanciar.");
     }
+
+    static listDailies(filter='', )
 }
+
+module.exports = DashboardController;
