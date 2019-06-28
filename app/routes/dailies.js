eb1de2046109ea293e810f379f82406d23329f01
Albano Borba <albano.b06@gmail.com>
2019-06-28 10:42:12 -0300

    (FIX) route instances

---
+++ app/routes/dailies.js
@@ -0,0 +1,34 @@
+let sessionCheckerRedLogin = require('../helper/sessionCheckerRedLogin');
+let controller = require('../controllers/DailiesController');
+
+module.exports = function(app)
+{
+    // route for user's dashboard
+    app.route('/daily')
+        .get((req, res) => {
+            
+            controller.listDailies()
+            res.render('dashboard');
+        })
+        .post(sessionCheckerRedLogin, (req,res) => {
+            // controller adicionar nova daily
+            
+            controller.addDaily(req)
+            .then(retorno => {
+                if(!retorno)
+                    res.send('erro');
+            })
+            .catch(retorno => {
+                if(!retorno)
+                    res.send('erro');
+            });
+        })
+        .delete(sessionCheckerRedLogin, (req,res) => {
+
+            // controller deletar daily
+        })
+        .put(sessionCheckerRedLogin, (req,res) => {
+
+            // controller alterar daily
+        })
+}
--- app/routes/dashboard.js
+++ app/routes/dashboard.js
@@ -12,25 +12,4 @@ module.exports = function(app)
             // console.log(req.session.user);
             res.render('dashboard');
         })
-        .post(sessionCheckerRedLogin, (req,res) => {
-            // controller adicionar nova daily
-            
-            controllerAdicionarDaily(req)
-            .then(retorno => {
-                if(!retorno)
-                    res.send('erro');
-            })
-            .catch(retorno => {
-                if(!retorno)
-                    res.send('erro');
-            });
-        })
-        .delete(sessionCheckerRedLogin, (req,res) => {
-
-            // controller deletar daily
-        })
-        .put(sessionCheckerRedLogin, (req,res) => {
-
-            // controller alterar daily
-        })
 }
