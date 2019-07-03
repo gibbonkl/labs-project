8915ab8dd1e1cc29fbe885cbf0bce76493a4b0a3
Diego Bastos da Costa <dbdcosta96@gmail.com>
2019-07-03 11:53:04 -0300

    (FIX) Permissões para alteração de daily

--- app/controllers/DailiesController.js
+++ app/controllers/DailiesController.js
@@ -3,35 +3,88 @@ var DailyDao = require('../infra/dao/DailyDao');
 
 let UserModel = require("../models/schema_usuario");
 var UserDAO = require('../infra/dao/UserDao');
-
+/*
+    *   Controller responsável por gerenciar as dailies
+    *   Busca todas por filtro, adiciona daily, remove daily,
+    *   edita e a retorna a paginação
+    * 
+    *   @note   Todos os retornos são por meio de Promise
+    *   @author Albano Borba
+    *   @edit Diego Bastos
+*/
 class DailiesController {
+    /*
+        *   Classe estática
+        *   @static
+    */
     constructor() {
         throw new Error("Classe estática. Impossível instanciar.");
     }
-
+    /*
+        *   Retorna a lista de daily
+        *   Caso a operação seja por usuário,
+        *   Retorna todas dailies para aquele usuário,
+        *   Caso a operação seja por data,
+        *   Retorna todas operações para aquela data
+        * 
+        *   @static
+        *   @param {Request} req Requisição do usuário
+        *   @param {string} op Operação solicitada pelo usuário
+        *   @param {Number} page Referente a paginação
+        *   @param {Number} batch Número máximo de dailies por retorno
+        *   @return {Array}    
+    */
     static listDailies(req, op, page=1, batch=20){
         let userDao = new UserDAO(UserModel);
         let dailyDao = new DailyDao(DailyModel);
         //verificar user
+        /*
+            *   Verifica o tipo de usuário
+            *   E o username
+        */
         let user = 'visitante'
         let username = ''
+        /*
+            *   Se existe um usuário na sessão,
+            *   Verifica as permissões e
+            *   Seta para a variável username
+            *   Caso contrário, realiza as operações como visitante
+        */
         if(req.session.user) {
-            user = userDao.checkUserPermission(req.session.user.username);
-            username = req.session.user.username;
-        }
+            if(req.session.user.tipo == "admin")
+                user = "admin"
+            else{
+                user = userDao.checkUserPermission(req.session.user.username);
+                username = req.session.user.username;
+            }
 
+        }
+        
          // get dailies
         if (op == 'user'){
+            /*
+                *   Se a operação for por usuário
+                *   Seta as permissões para true somente se o usuário for dono da daily
+                *   Ou se o usuário for admin do sistema
+                *   Se não, seta a permissão para false
+            */
             return dailyDao.listDailyNotesByUser(req.body.filtro, (page-1)*batch, batch) 
                 .then(dailies => 
-                    dailies.map(function(daily){ 
-                        if (user == 'admin' || daily.username == username)
-                            daily['permissao'] = true;
-                        return daily}))
+                    dailies.map((daily)=>{
+                        user == 'admin' || daily['usuario'] == username ? daily['permissao'] = true : daily['permissao'] = false
+                        return daily
+
+                    }))
                 .catch(console.error)
         }
 
         else if(op == 'data'){
+            /*
+                *   Se a operação for por data
+                *   Seta as permissões para true somente se o usuário for dono da daily
+                *   Ou se o usuário for admin do sistema
+                *   Se não, seta a permissão para false
+            */
             return dailyDao.listDailyNotesByDate(req.body.filtro, (page-1)*batch, batch) 
                 .then(dailies => 
                     dailies.map(daily=>{
@@ -43,7 +96,15 @@ class DailiesController {
         }
 
     }
-
+    /*
+        *   Insere uma lista na base de dados,
+        *   Só é possível inserir uma daily
+        *   por usuário para determinado dia
+        * 
+        *   Retorna a daily inserida no banco, se possível
+        *   @param {Request} req Requisição com as informações da daily
+        *   @return {object}
+    */
     static addDaily(req){
         
         let daily = new DailyModel({
@@ -61,14 +122,25 @@ class DailiesController {
             .then(response => response)
             .catch(console.error)
     }
-
+    /*
+        *   Remove uma daily da base dados
+        *   E seta o campo ativo para false
+        * 
+        *   @param {Request} req Requisição com o id para remoção
+        *   @return {object}
+    */
     static deleteDaily(req) {
         let dailyDao = new DailyDao(DailyModel);
         return dailyDao.removeDailyNoteById(req.body.daily_id)
             .then(response => response)
             .catch(console.error)
     }
-
+    /*
+        *   Edita uma daily na base de dados
+        * 
+        *   @param {Request} req Requisição com o corpo para modificação
+        *   @return {object}
+    */
     static updateDaily(req) {
 
         let dailyDao = new DailyDao(DailyModel);
@@ -82,7 +154,11 @@ class DailiesController {
             .then(response => response)
             .catch(console.error)
     }
-
+    /*
+        *   Verifica a paginação das dailies
+        * 
+        *   @return {Number}
+    */
     static numberOfDailies() {
         let dailyDao = new DailyDao(DailyModel);
 
