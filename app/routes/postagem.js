01ee0b8be0ba265e48c521eaf9922cb4ed65c2eb
Gibbon <gibbon@furg.br>
2019-07-08 10:59:29 -0300

    (FEAT) rota deletar postagem

--- app/routes/postagem.js
+++ app/routes/postagem.js
@@ -17,4 +17,14 @@ module.exports = function(app) {
                 .then(postagem => postagem ? res.send(postagem) : res.send("Não foi possível editar postagem"))
                 .catch(console.error)
         })
+
+
+    app.route('/testehelpCenter/:idpostagem')
+        //deletar postagem
+        .delete(sessionCheckerRedLogin, (req, res) => {
+            controller.deletarPostagem(req.params.idpostagem)
+                .then(postagem => postagem ? res.send(postagem) : res.send("Não foi possível deletr postagem"))
+                .catch(console.error)
+        })
 }
+
