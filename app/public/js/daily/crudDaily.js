f6626c35255e93ce1fc2cfe50a507beb58db8f6e
Albano <albano.b06@gmail.com>
2019-07-08 10:10:37 -0300

    (FEAT) funcao do front para listagem de posts

--- app/public/js/daily/crudDaily.js
+++ app/public/js/daily/crudDaily.js
@@ -61,8 +61,7 @@ function create() {
 }
 
 function render(dados) {
-    teste = dados
-    console.log(teste)
+    
     return `<li id="${dados._id}" class="data">
                 <div class="collapsible-header">
                     <i class="material-icons">face</i>
