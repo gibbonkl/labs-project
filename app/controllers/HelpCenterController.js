fb2165ea92a6bf44e815b7033760a8892e8d9654
Rafael Neves Romeu <rafael.romeu_BOLS@pampa.compasso>
2019-07-08 09:42:54 -0300

    (FIX) adiciona novos parametros

--- app/controllers/HelpCenterController.js
+++ app/controllers/HelpCenterController.js
@@ -25,29 +25,40 @@ class HelpCenterController {
                     postagem.map(function(postagem){
                         if (user == 'admin' || postagem.username == username)
                             postagem['permissao'] = true;
+                        HelpCenterController.insereLikesEComentarios(postagem);
                         return postagem}))
                 .catch(console.error)
         }
         else if(op == 'data')
         {
-            return postagemDao.listarPostagemByDate('4/7/2019', (page-1)*batch, batch)
+            return postagemDao.listarPostagemByDate(req.body.data, (page-1)*batch, batch)
                 .then(postagem => 
                     postagem.map(function(postagem){
                         if (user == 'admin' || postagem.username == username)
                             postagem['permissao'] = true;
+                        HelpCenterController.insereLikesEComentarios(postagem);
                         return postagem}))
                 .catch(console.error)
         }
         else if(op == 'username')
         {
-            return postagemDao.listarPostagemByUser('Goku', (page-1)*batch, batch)
+            return postagemDao.listarPostagemByUser(req.body.username, (page-1)*batch, batch)
                 .then(postagem => 
                     postagem.map(function(postagem){
                         if (user == 'admin' || postagem.username == username)
                             postagem['permissao'] = true;
+                        HelpCenterController.insereLikesEComentarios(postagem);
                         return postagem}))
                 .catch(console.error)
         }
+    }
+
+    static insereLikesEComentarios(postagem)
+    {
+        postagem['likes'] = []
+        postagem['comentarios'] = []
+        postagem['numeroLikes'] = postagem.likes.length;
+        postagem['numeroComentarios'] = postagem.comentarios.length;
     }
 
     static insertPostagem(req) {
