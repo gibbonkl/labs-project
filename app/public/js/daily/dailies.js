5bcb68d8db45abe4a670776ae73face01a8af9ab
Anna Zimmermann <annaflaviaz@gmail.com>
2019-07-09 10:41:03 -0300

    (FIX) menu.js e main.css

--- app/public/js/daily/dailies.js
+++ app/public/js/daily/dailies.js
@@ -1,12 +1,6 @@
 $(document).ready(function() {
     listDailies()
-    $(".dropdown-trigger").dropdown({
-        constrainWidth: false,
-        coverTrigger: false,
-        hover: true
-    });
     $('.collapsible').collapsible({
         accordion: false
     });
-
 });
