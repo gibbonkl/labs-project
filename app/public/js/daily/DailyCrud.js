61627cc48ff54079fb1ae621cb48d7382f1d4cd4
Rafael-Romeu <rafaelromeufurg@gmail.com>
2019-07-12 22:06:17 -0300

    (FIX) requisicao ouvir resposta

--- app/public/js/daily/DailyCrud.js
+++ app/public/js/daily/DailyCrud.js
@@ -1,23 +1,173 @@
-import { DailyController } from './DailyController.js';
-
-class DailyCrud
+function inserir()
 {
-    constructor()
-    {
-        this.dailyController = new DailyController();
+    Swal.mixin({
+        input: 'text',
+        confirmButtonText: 'Próximo &rarr;',
+        reverseButtons: true,
+        showCancelButton: true,
+        cancelButtonColor: '#b3bac5',
+        cancelButtonText: 'Cancelar',
+        allowOutsideClick: false,
+        progressSteps: ['1', '2', '3'],
+        inputValidator: (value) => {
+            if (!value) {
+                return 'Você precisa preencher o campo!'
+            }
+        }
+    }).queue([{
+            title: 'O que você fez ontem?',
+            text: 'Cadastrar daily'
+        },
+        {
+            title: 'O que você fará hoje?',
+            text: 'Cadastrar daily'
+        },
+        {
+            title: 'Há algum impedimento?',
+            text: 'Cadastrar daily'
+        }
+    ])
+    .then( result => {
+        return {
+            'ontem': result.value[0],
+            'hoje': result.value[1],
+            'impedimento': result.value[2]
+        }
+    })
+    .then(resultado => {
+
+        fetch("/daily", {
+            method: "POST",
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(resultado)
+         })
+        .then(response => response.json())
+        .then(response => {
+
+            if(response.erro)
+                message('error', response.erro);
+            else
+            {
+                adicionarDailyDOM(response);
+                message('succes', 'Daily registrada!')
+            }
+        })
+        .catch(message('error', 'Unexpected Error'))
+    })
+    .catch(() => {console.log});
+        
+}
+
+
+
+function deletar(id)
+{
+    Swal.fire({
+        title: 'Tem certeza que deseja excluir a daily?',
+        text: "Você não poderá reverter",
+        type: 'warning',
+        reverseButtons: true,
+        showCancelButton: true,
+        confirmButtonColor: '#d33',
+        confirmButtonText: 'Deletar',
+        cancelButtonColor: '#b3bac5',
+        cancelButtonText: 'Cancelar',
+        allowOutsideClick: false
+    })
+    .then(resp => {
+        if (resp.value) {
+            fetch("/daily", {
+                method: "DELETE",
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ daily_id: id })
+            })
+            .then(response => response.json())
+            .then(response => {
+
+                if(response.erro)
+                    message('error', response.erro)
+                else
+                {
+                    removerElementDOM(id);
+                    message('succes', 'A Sua Daily Foi Deletada!')
+                }
+            })
+            .catch(message('error', 'Unexpected Error'))
+            
+        }
+    })
+    .catch(console.log)
+}
+
+function listar()
+{
+    fetch("/daily/data", {
+        method: "POST",
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            "filtro": dateConverter()
+        })
+    })
+    .then(response => response.json())
+    .then(dailies => {
+        listarDailiesDOM(dailies);
+    })
+    .catch(console.log)
+}
+
+function listarPorData(data)
+{
+    fetch("/daily/data", {
+        method: "POST",
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            filtro: data
+        })
+    })
+    .then(response => response.json())
+    .then(dailies => listarDailiesDOM(dailies))
+}
+
+function listarPorUser(name)
+{
+    fetch("/daily/user", {
+        method: "POST",
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            filtro:name
+        })
+    })
+    .then(response => response.json())
+    .then(dailies => {
+        listarDailiesDOM(dailies);
+    })
+}
+
+function filtrar(){
+    let data = $("input[name='filter_data']").val();
+    let name = $("input[name='filter_username']").val();
+    if(name.length) listarPorUser(name)
+    else data.length? listarPorData(data) : listar()
+
+}
+
+function editar(id)
+{
+    var obj = {
+        ontem: $("#" + id + " .ontem").html(),
+        hoje: $("#" + id + " .hoje").html(),
+        imp: $("#" + id + " .imp").html()
     }
 
-    inserir()
-    {
-        Swal.mixin({
+    Swal.mixin({
             input: 'text',
             confirmButtonText: 'Próximo &rarr;',
-            reverseButtons: true,
             showCancelButton: true,
+            progressSteps: ['1', '2', '3'],
             cancelButtonColor: '#b3bac5',
             cancelButtonText: 'Cancelar',
             allowOutsideClick: false,
-            progressSteps: ['1', '2', '3'],
+            reverseButtons: true,
             inputValidator: (value) => {
                 if (!value) {
                     return 'Você precisa preencher o campo!'
@@ -25,230 +175,64 @@ class DailyCrud
             }
         }).queue([{
                 title: 'O que você fez ontem?',
-                text: 'Cadastrar daily'
+                text: 'Editar daily',
+                inputValue: obj.ontem
             },
             {
                 title: 'O que você fará hoje?',
-                text: 'Cadastrar daily'
+                text: 'Editar daily',
+                inputValue: obj.hoje
             },
             {
                 title: 'Há algum impedimento?',
-                text: 'Cadastrar daily'
+                text: 'Editar daily',
+                inputValue: obj.imp
             }
-        ]).then((result) => {
-            if (result.value) {
-                Swal.fire({
-                    position: 'center',
-                    type: 'success',
-                    title: 'Daily registrada!',
-                    showConfirmButton: true,
-                    timer: 1500
-                })
-                return {
-                    'ontem': result.value[0],
-                    'hoje': result.value[1],
-                    'impedimento': result.value[2]
-                }
-            } else {
-                return false;
+        ])
+        .then((result) => {
+            return {
+                'ontem': result.value[0],
+                'hoje': result.value[1],
+                'impedimento': result.value[2],
+                '_id': id
             }
-        })
-        .then(resultado =>
-            fetch("/daily", {
-                method: "POST",
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify(resultado)
-            })
-        )
-        .then(response => response.json())
-        .then(response => {response['permissao'] = true; return response})
-        .then(response => this.dailyController.adicionarDailyDOM(response))
-        .catch(console.log);
-    }
-
-    editar(id)
-    {
-        var obj = {
-            ontem: $("#" + id + " .ontem").html(),
-            hoje: $("#" + id + " .hoje").html(),
-            imp: $("#" + id + " .imp").html()
-        }
-    
-        Swal.mixin({
-                input: 'text',
-                confirmButtonText: 'Próximo &rarr;',
-                showCancelButton: true,
-                progressSteps: ['1', '2', '3'],
-                cancelButtonColor: '#b3bac5',
-                cancelButtonText: 'Cancelar',
-                allowOutsideClick: false,
-                reverseButtons: true,
-                inputValidator: (value) => {
-                    if (!value) {
-                        return 'Você precisa preencher o campo!'
-                    }
-                }
-            }).queue([{
-                    title: 'O que você fez ontem?',
-                    text: 'Editar daily',
-                    inputValue: obj.ontem
-                },
-                {
-                    title: 'O que você fará hoje?',
-                    text: 'Editar daily',
-                    inputValue: obj.hoje
-                },
-                {
-                    title: 'Há algum impedimento?',
-                    text: 'Editar daily',
-                    inputValue: obj.imp
-                }
-            ])
-            .then((result) => {
-                return {
-                    'ontem': result.value[0],
-                    'hoje': result.value[1],
-                    'impedimento': result.value[2],
-                    '_id': id
-                }
-            })
-            .then(resp => {
-                fetch("/daily", {
-                    method: "PUT",
-                    headers: { 'Content-Type': 'application/json' },
-                    body: JSON.stringify(resp)
-                })
-                .then(resp => {
-
-                    $("#" + id + " .ontem").html(resp.ontem);
-                    $("#" + id + " .hoje").html(resp.hoje);
-                    $("#" + id + " .imp").html(resp.impedimento);
-
-                    Swal.fire({
-                        position: 'center',
-                        type: 'success',
-                        title: 'Daily Editada!',
-                        showConfirmButton: true,
-                        timer: 1500
-                    })
-                })
-                .catch(() => {
-
-                    Swal.fire({
-                        position: 'center',
-                        type: 'error',
-                        title: 'Não Foi Possível Editar a Daily no Momento!',
-                        showConfirmButton: true,
-                        timer: 1500
-                    })
-                })
-                
-            })
-            .catch(resp => {
-                Swal.close();
-            })
-    }
-
-    deletar(id)
-    {
-        Swal.fire({
-            title: 'Tem certeza que deseja excluir a daily?',
-            text: "Você não poderá reverter",
-            type: 'warning',
-            reverseButtons: true,
-            showCancelButton: true,
-            confirmButtonColor: '#d33',
-            confirmButtonText: 'Deletar',
-            cancelButtonColor: '#b3bac5',
-            cancelButtonText: 'Cancelar',
-            allowOutsideClick: false
         })
         .then(resp => {
-            if (resp.value) {
-                fetch("/daily", {
-                    method: "DELETE",
-                    headers: { 'Content-Type': 'application/json' },
-                    body: JSON.stringify({ daily_id: id })
-                })
-                .then(resp => {
 
-                    this.dailyController.removerElementDOM(id);
-                    Swal.fire(
-                        'Excluida!',
-                        'A sua daily foi deletada!',
-                        'success'
-                    )
-                })
-                .catch(() => {
-
-                    
-                    Swal.fire({
-                        position: 'center',
-                        type: 'error',
-                        title: 'Não Foi Possível Deletar A Daily No Momento!',
-                        showConfirmButton: true,
-                        timer: 1500
-                    })
-                })
-                return resp;
-            }
-        })
-        .then((result) => {
-            if (result.value) {
+            fetch("/daily", {
+                method: "PUT",
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(resp)
+            })
+            .then(response => response.json())
+            .then(response => {
                 
-            }
-        })
-        .catch((result) => {
-            Swal.close();
-        })
-    }
-
-    listar()
-    {
-        fetch("/daily/data", {
-            method: "POST",
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({
-                "filtro": this.dailyController.dateConverter()
+                console.log(response.corpo.ontem);
+                console.log($("#" + id + " .ontem").html());
+                if(response.erro)
+                    message('error', response.erro);
+                else
+                {
+                    $("#" + id + " .ontem").html(response.corpo.ontem);
+                    $("#" + id + " .hoje").html(response.corpo.hoje);
+                    $("#" + id + " .imp").html(response.corpo.impedimento);
+                    message('succes', 'Daily Editada!');
+                }
+                
             })
+            .catch(message('error', 'Unexpected Error'))
+            
         })
-        .then(response => response.json())
-        .then(dailies => {
-            console.log(dailies);
-            this.dailyController.listarDailiesDOM(dailies);
-        })
-        .catch(console.log)
-    }
-
-    listarPorData()
-    {
-        fetch("/daily/data", {
-            method: "POST",
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({
-                filtro: data
-            })
-        })
-        .then(response => response.json())
-        .then(dailies => {
-            this.dailyController.listarDailiesDOM(dailies);
-        })
-    }
-
-    listarPorUser()
-    {
-        fetch("/daily/user", {
-            method: "POST",
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({
-                filtro:name
-            })
-        })
-        .then(response => response.json())
-        .then(dailies => {
-            this.dailyController.listarDailiesDOM(dailies);
-        })
-    }
+        .catch(() => {console.log});
 }
 
-export { DailyCrud };
+function message(type, title)
+{
+    Swal.fire({
+        position: 'center',
+        type: type,
+        title: title,
+        showConfirmButton: true,
+        timer: 1500
+    })
+}
--- app/public/js/daily/DailyListener.js
+++ app/public/js/daily/DailyListener.js
@@ -1,27 +1,4 @@
-import { DailyController } from './DailyController.js';
-import { DailyCrud } from './DailyCrud.js';
-
 $(document).ready(function() {
-    //listDailies();
-    var dailyCrud = new DailyCrud();
-    dailyCrud.listar();
-
-    var dailyController = new DailyController();
-    //dailyController.datePicker();
-    
-    $("#radio-one").click( () => console.log('radio one') );
-    $("#radio-two").click( () => console.log('radio two')  );
-    $("#filtro").click( () => {
-        console.log($("i[name='deletarDaily']")) 
-        // let data = $("input[name='filter_data']").val();
-        // let name = $("input[name='filter_username']").val();
-        // if(name.length) this.dailyCrud.listarPorUser(name)
-        // else data.length ? this.dailyCrud.listarPorData(data) : this.dailyCrud.listar()
-    });
-    $("#inserir").click( () => console.log('inserir')  );
-    
-    $("i[name='deletarDaily']").click( () => {
-        console.log('deletar' + $(this).closest('li').attr('id'));
-    });
-    //$("#editar").click(this.editar( $("#editar").closest('ul').attr('id')) );
+    listar();
+    datePicker();
 });
