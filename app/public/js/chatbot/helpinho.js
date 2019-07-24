29ce8db4e6a546d08be7106e1d725b5c2bebba46
AlbanoBorba <albano.b06@gmail.com>
2019-07-24 11:46:25 -0300

    (FEAT) helpinho identifica no front mensagens do user e suas proprias respostas

--- app/public/js/chatbot/helpinho.js
+++ app/public/js/chatbot/helpinho.js
@@ -1,15 +1,25 @@
+const store = window.WebChat.createStore(
+    {},
+    ({ dispatch }) => next => action => {
+        if (action.type === 'DIRECT_LINE/INCOMING_ACTIVITY') {
+        const event = new Event('webchatincomingactivity');
 
-window.WebChat.renderWebChat({
-        directLine: window.WebChat.createDirectLine({
-            token: '3y5fPZhwLfE.ASfIdTbH0uY9l1gl_oNZthZ4J96T5M_S6HssbA8Nty4'
-        }),
-        userID: '007',
-        username: 'James Bond',
-        locale: 'en-US',
-        botAvatarInitials: 'WC',
-        userAvatarInitials: 'WW'
-    },
-    document.getElementById('webchat')
+        event.data = action.payload.activity;
+        window.dispatchEvent(event);
+        }
+
+    return next(action);
+    }
 );
 
+window.WebChat.renderWebChat({
+    directLine: window.WebChat.createDirectLine({ token: '3y5fPZhwLfE.ASfIdTbH0uY9l1gl_oNZthZ4J96T5M_S6HssbA8Nty4' }), store
+}, document.getElementById('webchat'));
 
+window.addEventListener('webchatincomingactivity', ({ data }) => {         
+    //console.log(`Received an activity of type "${ data.type }":`);
+    console.log('Cpx falou:')
+    console.log(data);       
+});
+
+document.querySelector('#webchat > *').focus();
