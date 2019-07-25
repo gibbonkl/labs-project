ba8564e74b55e134c3a1811b35fd915c8160bf13
AlbanoBorba <albano.b06@gmail.com>
2019-07-25 08:30:05 -0300

    (FIX) teste novo bot

--- app/public/js/chatbot/helpinho.js
+++ app/public/js/chatbot/helpinho.js
@@ -12,14 +12,17 @@ const store = window.WebChat.createStore(
     }
 );
 
+window.addEventListener('webchatincomingactivity', ({ data }) => {         
+    //console.log(`Received an activity of type "${ data.type }":`);
+    //console.log('Cpx falou:')
+    //console.log(data);
+    if (data.text = 'log'){
+        alert('foi!');
+    }
+});
+
 window.WebChat.renderWebChat({
     directLine: window.WebChat.createDirectLine({ token: '3y5fPZhwLfE.ASfIdTbH0uY9l1gl_oNZthZ4J96T5M_S6HssbA8Nty4' }), store
 }, document.getElementById('webchat'));
-
-window.addEventListener('webchatincomingactivity', ({ data }) => {         
-    //console.log(`Received an activity of type "${ data.type }":`);
-    console.log('Cpx falou:')
-    console.log(data);       
-});
 
 document.querySelector('#webchat > *').focus();
