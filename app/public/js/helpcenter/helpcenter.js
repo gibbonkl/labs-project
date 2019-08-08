abd2994f9d9b328b1e28f003ee76a689e4635163
Rafael Neves Romeu <rafael.romeu_BOLS@pampa.compasso>
2019-08-08 08:47:12 -0300

    (FIX) tags

--- app/public/js/helpcenter/helpcenter.js
+++ app/public/js/helpcenter/helpcenter.js
@@ -40,7 +40,7 @@ function render(dados) {
             </div>
             <div class="tags">
             <div class="${dados.tags[0].length ? `right` : `hide` }"> 
-            ${dados.tags.length ? dados.tags[0].split(',').map( element => `<span class="chip">${removeTags(element)}</span>`).join('') : ''}           
+            ${dados.tags.length ? dados.tags.map( element => `<span class="chip">${removeTags(element)}</span>`).join('') : ''}           
             </div>
             &nbsp;
                 </div>
